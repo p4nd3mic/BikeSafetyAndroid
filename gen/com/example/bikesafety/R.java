579e1574b829ff012b7bc7217474d3052f69dcbe
AParks <thalia1.618@gmail.com>
2013-04-09 18:03:16 -0400

    legend and splash screen

--- gen/com/example/bikesafety/R.java
+++ gen/com/example/bikesafety/R.java
@@ -128,26 +128,30 @@ containing a value of this type.
         public static final int zOrderOnTop=0x7f01000d;
     }
     public static final class drawable {
-        public static final int bike_icon=0x7f020000;
-        public static final int bikesafety_icon=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
+        public static final int bikewell=0x7f020000;
+        public static final int caution=0x7f020001;
+        public static final int cycling=0x7f020002;
+        public static final int cycling_covered=0x7f020003;
+        public static final int splash_screen=0x7f020004;
     }
     public static final class id {
         public static final int listview=0x7f040004;
-        public static final int map=0x7f040006;
-        public static final int menu_photo_creds=0x7f040009;
-        public static final int menu_settings=0x7f040008;
+        public static final int map=0x7f040007;
+        public static final int map_container=0x7f040006;
+        public static final int menu_photo_creds=0x7f04000a;
+        public static final int menu_settings=0x7f040009;
         public static final int none=0x7f040000;
         public static final int normal=0x7f040001;
         public static final int progress_bar=0x7f040005;
         public static final int satellite=0x7f040002;
         public static final int terrain=0x7f040003;
-        public static final int textView1=0x7f040007;
+        public static final int textView1=0x7f040008;
     }
     public static final class layout {
         public static final int activity_comment=0x7f030000;
         public static final int activity_main=0x7f030001;
         public static final int safety_tips=0x7f030002;
+        public static final int splash_screen=0x7f030003;
     }
     public static final class menu {
         public static final int activity_main=0x7f070000;
