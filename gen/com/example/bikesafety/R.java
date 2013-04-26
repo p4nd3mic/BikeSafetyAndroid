2cbcb297ab49756ba09eeb7a4b12fb917ea30967
AParks <thalia1.618@gmail.com>
2013-04-26 10:59:48 -0400

    merge

--- gen/com/example/bikesafety/R.java
+++ gen/com/example/bikesafety/R.java
@@ -135,24 +135,26 @@ containing a value of this type.
         public static final int splash_screen=0x7f020004;
     }
     public static final class id {
-        public static final int address=0x7f05000d;
-        public static final int button1=0x7f05000a;
-        public static final int button2=0x7f05000b;
+        public static final int address=0x7f05000f;
+        public static final int button1=0x7f05000c;
+        public static final int button2=0x7f05000d;
+        public static final int info=0x7f05000a;
         public static final int list=0x7f050008;
         public static final int listview=0x7f050004;
         public static final int map=0x7f050009;
         public static final int map_container=0x7f050006;
-        public static final int menu_photo_creds=0x7f050010;
-        public static final int menu_search=0x7f050011;
-        public static final int menu_settings=0x7f05000f;
+        public static final int menu_photo_creds=0x7f050012;
+        public static final int menu_search=0x7f050013;
+        public static final int menu_settings=0x7f050011;
         public static final int none=0x7f050000;
         public static final int normal=0x7f050001;
         public static final int progress_bar=0x7f050005;
         public static final int satellite=0x7f050002;
-        public static final int search_entry=0x7f05000c;
+        public static final int search_entry=0x7f05000e;
         public static final int terrain=0x7f050003;
         public static final int text=0x7f050007;
-        public static final int textView1=0x7f05000e;
+        public static final int textView1=0x7f050010;
+        public static final int tv=0x7f05000b;
     }
     public static final class layout {
         public static final int activity_comment=0x7f030000;
